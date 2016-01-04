dc7eb162f164d1db6def4c82ad2e2755c677191f
Esra Neuhofer <esra.neuhofer@yahoo.de>
2016-01-04 14:40:04 +0100

    Last change b

--- server/db.js
+++ server/db.js
@@ -1,14 +1,17 @@
 var Sequelize = require('sequelize');
-// var env = process.env.NODE_ENV || 'development';
+var env = process.env.NODE_ENV || 'development';
 var sequelize;
 
-
+if (env === 'production') {
+	sequelize = new Sequelize(process.env.DATABASE_URL, {
+		dialect: 'postgres'
+	});
+} else {
 	sequelize = new Sequelize(undefined, undefined, undefined, {
 		'dialect': 'sqlite',
 		'storage': __dirname + '/data/gerichte-api.sqlite'
 	});
-
-
+}
 var db ={};
 
 db.gerichte = sequelize.import(__dirname +'/models/gerichte.js');
