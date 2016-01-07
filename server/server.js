5fe4abd4d7e526bac91b11d5c7b797f93a1c2da5
Esra Neuhofer <esra.neuhofer@yahoo.de>
2016-01-07 10:09:14 +0100

    Datebase functional

--- server/server.js
+++ server/server.js
@@ -206,7 +206,7 @@ app.get('/',function(req, res){
 // 	res.sendFile('index.html',{root:'../public/html'});
 // });
 
-db.sequelize.sync({force:true}).then(function() {
+db.sequelize.sync().then(function() {
 	app.listen(PORT, function() {
 		console.log('Express listening on port ' + PORT + '!');
 	});
